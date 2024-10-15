6ac0dffcaba33b5d46201091dfc95c00c8e0667d
qrkks <qrkks3@gmail.com>
2024-10-15 16:00:26 +0800

    2024-10-15 15:59:59 before move workoutset

--- frontend/app/workouts/[date]/components/ExerciseSelectInput.jsx
+++ frontend/app/workouts/[date]/components/ExerciseSelectInput.jsx
@@ -1,38 +1,39 @@
 import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectLabel,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select";
-  import {useState} from "react";
-  
-  export default function SelectInput({placeholder, entries, mutate, selectedExercise, onSelectChange}) {
-  
-  if (selectedExercise) {
-  console.log("selectedExercise", selectedExercise);}
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {useState} from "react";
 
-    return (
-      <Select value={selectedExercise} onValueChange={onSelectChange} >
-        <SelectTrigger>
-          <SelectValue placeholder={placeholder} />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>{placeholder}</SelectLabel>
-            {entries?.map((entry) => (
-              <SelectItem key={entry.id} value={entry.name}>
-                {entry.name}
-              </SelectItem>
-            ))}
-            <SelectItem key="new" value="new">
-              新建...
+export default function SelectInput({
+  placeholder,
+  entries,
+  mutate,
+  selectedExercise,
+  onSelectChange,
+}) {
+  return (
+    <Select value={selectedExercise} onValueChange={onSelectChange}>
+      <SelectTrigger>
+        <SelectValue placeholder={placeholder} />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>{placeholder}</SelectLabel>
+          {entries?.map((entry) => (
+            <SelectItem key={entry.id} value={entry.name}>
+              {entry.name}
             </SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-    );
-  }
-  
+          ))}
+          <SelectItem key="new" value="new">
+            新建...
+          </SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
