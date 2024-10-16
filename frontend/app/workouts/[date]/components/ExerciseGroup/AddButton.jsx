72818f881b579dcd5b249a3383ac0f3f21497de0
qrkks <qrkks3@gmail.com>
2024-10-17 07:50:52 +0800

    2024-10-17 07:50:51

--- frontend/app/workouts/[date]/components/ExerciseGroup/AddButton.jsx
+++ frontend/app/workouts/[date]/components/ExerciseGroup/AddButton.jsx
@@ -1,8 +1,26 @@
 import {CirclePlus} from "lucide-react";
 import SheetContainer from "@/components/SheetContainer";
 import useSWR from "swr";
-import {useEffect, useState} from "react";
+import {use, useEffect, useState} from "react";
+import {Input} from "@/components/ui/input";
+
 function AddButton() {
+  const [formData, setFormData] = useState({
+    weight: "",
+    reps: "",
+  });
+
+  function handleChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  useEffect(() => {
+    console.log("formData", formData);
+  }, [formData]);
+
   return (
     <>
       <SheetContainer
@@ -15,7 +33,24 @@ function AddButton() {
         }
         submitButtonText="确定"
       >
-        <form className="flex items-center w-full space-x-2">form</form>
+        <form className="flex flex-col items-center justify-center w-full gap-2">
+          <Input
+            name="weight"
+            type="number"
+            min="0"
+            placeholder="Weight"
+            value={formData.weight || ""}
+            onChange={handleChange}
+          />
+          <Input
+            name="reps"
+            type="number"
+            min="0"
+            placeholder="Reps"
+            value={formData.reps || ""}
+            onChange={handleChange}
+          />
+        </form>
       </SheetContainer>
     </>
   );
--- frontend/app/workouts/[date]/components/ExerciseGroup/ExerciseSet.jsx
+++ frontend/app/workouts/[date]/components/ExerciseGroup/ExerciseSet.jsx
@@ -1,15 +1,20 @@
-import AddButton from "./AddButton"
-import ExerciseItem from "./ExerciseItem"
+import AddButton from "./AddButton";
+import RemoveButton from "./RemoveButton";
+import ExerciseItem from "./ExerciseItem";
 
 function ExerciseSet({set}) {
-    return (<>
-        <div className="flex items-center gap-2">
-            {set.exercise.name}
-            <AddButton/>
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex items-center gap-2">
+        {set.exercise.name}
+        <div className="flex items-center gap-1">
+          <RemoveButton />
+          <AddButton />
         </div>
-        <ExerciseItem/>
-    </>
-    )
+      </div>
+      <ExerciseItem />
+    </div>
+  );
 }
 
-export default ExerciseSet
+export default ExerciseSet;
