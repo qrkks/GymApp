2e7af27740ff0fef87f685fdeb0ca4a6e70ba710
qrkks <qrkks3@gmail.com>
2024-10-17 20:33:21 +0800

    2024-10-17 20:33:19

---
+++ frontend/app/loading.js
@@ -0,0 +1,9 @@
+function loading() {
+  return (
+    <div className="flex items-start justify-center h-screen text-gray-400 pt-[30vh]">
+      Fetching data ...
+    </div>
+  );
+}
+
+export default loading;
