b6b78a036f750ab9acc518a7cef455dcef428f0f
qrkks <qrkks3@gmail.com>
2024-10-21 19:39:27 +0800

    2024-10-21 19:38:53 remove searchbar;fix exercise index exercise.map

--- frontend/app/exercise-library/Exercises/index.jsx
+++ frontend/app/exercise-library/Exercises/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import {useState, useEffect} from "react";
 import authStore from "@/app/store/authStore";
 import RemoveButton from "./RemoveButton";
 import useSWR from "swr";
@@ -6,34 +6,36 @@ import EditPopover from "./EditPopover";
 import config from "@/utils/config";
 
 function index({part}) {
+  const {apiUrl} = config;
 
-    const {apiUrl} = config
+  const {
+    data: exercises,
+    error,
+    mutate,
+  } = useSWR(`${apiUrl}/exercise?body_part_name=${part.name}`, (url) =>
+    fetch(url, {
+      credentials: "include",
+      headers: {"X-CSRFToken": authStore.getCookie("csrftoken")},
+    }).then((res) => res.json())
+  );
 
-    const {data: exercises, error, mutate} = useSWR(
-        `${apiUrl}/exercise?body_part_name=${part.name}`,
-        (url) =>
-            fetch(url, {
-                credentials: "include",
-                headers: {"X-CSRFToken": authStore.getCookie("csrftoken")},
-            }).then((res) => res.json())
-    );
+  if (error) return <div>Failed to load</div>;
 
-    if (error) return <div>Failed to load</div>;
-
-    return (
-        <ul className="flex flex-col items-center gap-2">
-            {exercises?.map((exercise) => (
-                <li key={exercise.id} className="flex items-center gap-2">
-                    <p>{exercise.name}</p>
-                    <div className="flex items-center gap-1">
-                    <RemoveButton exercise={exercise} mutate={mutate} />
-                    <EditPopover exercise={exercise} mutate={mutate} />
-                    </div>
-                </li>
-            ))}
-
-        </ul>
-    )
+  return (
+    <ul className="flex flex-col items-center gap-2">
+      {Array.isArray(exercises) &&
+        exercises.length > 0 &&
+        exercises.map((exercise) => (
+          <li key={exercise.id} className="flex items-center gap-2">
+            <p>{exercise.name}</p>
+            <div className="flex items-center gap-1">
+              <RemoveButton exercise={exercise} mutate={mutate} />
+              <EditPopover exercise={exercise} mutate={mutate} />
+            </div>
+          </li>
+        ))}
+    </ul>
+  );
 }
 
-export default index
+export default index;
