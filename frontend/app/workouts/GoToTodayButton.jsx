b2725ec719f25843e847ee917a4ae10fbccfef28
qrkks <qrkks3@gmail.com>
2024-10-21 19:22:13 +0800

    2024-10-21 19:22:10

--- frontend/app/workouts/GoToTodayButton.jsx
+++ frontend/app/workouts/GoToTodayButton.jsx
@@ -1,20 +1,18 @@
 "use client";
 import {useState} from "react";
 import {Button} from "@/components/ui/button";
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
+export function getTodayDate() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0"); // 月份从0开始，需加1
+  const day = String(today.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
 
 function StartWorkout({onStart}) {
   const [resData, setResData] = useState({});
   const router = useRouter();
-
-  function getTodayDate() {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, '0'); // 月份从0开始，需加1
-    const day = String(today.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-  }
-  
 
   const handleClick = () => {
     router.push(`/workouts/${getTodayDate()}`);
