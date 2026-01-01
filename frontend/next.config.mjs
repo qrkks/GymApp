76d6c6fe22046028bc20e5f4c60b21c5e86d6c17
qrkks <qrkks3@gmail.com>
2026-01-02 00:57:08 +0800

    u

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "standalone",
+  experimental: {
+    serverComponentsExternalPackages: ['better-sqlite3'],
+  },
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push('better-sqlite3');
+    }
+    return config;
+  },
 };
+
 
 export default nextConfig;
