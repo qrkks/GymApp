b0f0f3d1923ac692612e46323b01e7fcb32dd4e6
qrkks <qrkks3@gmail.com>
2026-01-01 22:03:34 +0800

    写死api config

--- frontend/utils/config.ts
+++ frontend/utils/config.ts
@@ -1,8 +1,10 @@
 // config.js
 const config = {
   // Use internal API routes if no external API URL is provided
-  apiUrl: process.env.NEXT_PUBLIC_API_URL || '/api',
-  rootUrl: process.env.NEXT_PUBLIC_ROOT_URL || '',
+  // apiUrl: process.env.NEXT_PUBLIC_API_URL || '/api',
+  apiUrl:  '/api',
+  // rootUrl: process.env.NEXT_PUBLIC_ROOT_URL || '',
+  rootUrl: '',
 };
 
 export default config;
